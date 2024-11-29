91b729f4710ee35d5ae6933367dd65a1dec69d10
Guilhermefariah <Guilhermefaria321@gmail.com>
2024-11-29 18:57:41 -0300

    styles: Adicionando tailwind para o projeto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.template')
+
+@section('content')
+    <h1 class="text-3xl font-bold text-red-600 underline">CRUD com Laravel</h1>
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
