b58686730835d52c095b26d0424997f96dbda4c6
Guilhermefariah <Guilhermefaria321@gmail.com>
2024-12-01 21:29:37 -0300

    Adicionado método PUT ao CRUD para atualizar recursos

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,7 +2,13 @@
 
 @section('content')
     <div class="m-auto">
-        <h1 class="text-center text-white font-bold text-3xl">Cadastrar</h1>
+        <h1 class="text-center text-white font-bold text-3xl">
+            @if (isset($book))
+                Editar
+            @else
+                Cadastrar
+            @endif 
+        </h1>
         <hr>
     </div>
 
@@ -16,30 +22,32 @@
             </div>
         @endif
 
-        <form name="formCard" id="formCard" action="{{ url('books') }}" method="POST">
+        @if (isset($book))
+            <form name="formEdit" id="formEdit" action="{{ url("books/$book->id") }}" method="POST">
+            @method('PUT')
+        @else
+            <form name="formCard" id="formCard" action="{{ url("books") }}" method="POST">
+        @endif
             @csrf
             <div class="form-group">
                 <label for="title">Título</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="Insira o título"
-                    required>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Insira o título" value="{{$book->title ?? ''}}" required>
             </div>
             <select class="form-control" name="id_user" id="id_user" required>
-                <option value="#">Autor</option>
+                <option value="{{$book->relUsers->id ?? ''}}">{{$book->relUsers->name ?? ''}}</option>
                 @foreach ($users as $user)
                     <option value="{{ $user->id }}">{{ $user->name }}</option>
                 @endforeach
             </select>
             <div class="form-group">
                 <label for="pages">Páginas</label>
-                <input type="number" class="form-control" id="pages" name="pages"
-                    placeholder="Insira o número de páginas" required>
+                <input type="number" class="form-control" id="pages" name="pages" placeholder="Insira o número de páginas" value="{{$book->pages ?? ''}}" required>
             </div>
             <div class="form-group">
                 <label for="price">Preço</label>
-                <input type="number" class="form-control" id="price" name="price" placeholder="Insira o preço"
-                    required>
+                <input type="number" class="form-control" id="price" name="price" placeholder="Insira o preço" value="{{$book->price ?? ''}}" required>
             </div>
-            <button type="submit" class="btn btn-dark">Cadastrar</button>
+            <button type="submit" class="btn btn-dark">@if (isset($book)) Editar @else Cadastrar @endif</button>
         </form>
     </div>
 @endsection
